60d4048d34320520f9bd13cb990d8d8fe1db74a5
sirrulham97 <ehlezham11@gmail.com>
2026-05-15 22:18:56 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+unsigned short num2=88945;
+int num1=3465891;
+cout<<"\n \n value of var1: " <<num1<<endl;
+cout<<"\n \n value of var2: " <<num2<<endl;
+return 0;}
